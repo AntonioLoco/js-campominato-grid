4ce792bf2be46f398805a50ca52b6e277798f2ca
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-10-17 15:30:15 +0200

    completed the basic exercise

--- js/script.js
+++ js/script.js
@@ -4,3 +4,64 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
+
+const btnPlay = document.getElementById("btn-play");
+const gridSquare = document.querySelector(".grid");
+
+// Genero un array di numeri
+let arrayNumbers = [];
+arrayNumbers = generateNumbersArray(100);
+
+// Al click dell bottone generiamo la griglia
+btnPlay.addEventListener("click", generateGrid);
+
+
+
+
+
+// FUNCTION
+/**
+ * Description: Funzione che genera un array di lunghezza pari ad arrayLenght rimpita con numeri progressivi da 1 a arrayLenght
+ * @param {number} arrayLenght la lunghezza dell'array da creare
+ * @returns {Object} Ritorna un array con i numeri che vanno progressivamente da 1 a arrayLenght
+ */
+function generateNumbersArray(arrayLength){
+    let arrayGenerate = [];
+    for(let i = 0; i < arrayLength; i++){
+        arrayGenerate.push(i + 1);
+    }
+    return arrayGenerate;
+}
+
+
+
+// UI FUNCTION
+/**
+ * Description: Funzione che ci crea l'elemento square
+ * @returns {object} ritorna l'elemento square creato
+ */
+function generateSquare(){
+    const generateSquare = document.createElement("div");
+    generateSquare.classList.add("square");
+    
+    return generateSquare;
+}
+
+
+/**
+ * Description: Funzione che genera la griglia di quadrati
+ */
+function generateGrid(){
+    // Generiamo la griglia
+    for(let i = 0; i < arrayNumbers.length ; i++){
+        const item = generateSquare();
+        item.innerHTML = arrayNumbers[i];
+        
+        item.addEventListener("click", function(){
+            item.classList.add("active");
+            console.log(item.innerHTML);
+        })
+
+        gridSquare.append(item);
+    }
+}
